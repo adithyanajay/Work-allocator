1d3216151ec61fb7dddaff0a7b83ef1fae6faa15
Harigovind M G <harigovindmg@gmail.com>
2023-10-26 01:00:23 +0530

    basic design for dashboard

--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Navbar from "../components/Navbar";
 import { TopBar } from "../components/TopBar";
+import { DashBoard } from "../components/DashBoard";
 
 function Home() {
   return (
     <div>
       <TopBar/>
+      <DashBoard/>
     </div>
   );
 }
