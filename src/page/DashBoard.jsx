f505b509d0f321c77f9e2bd86ead474976270750
adithyanajay@gmail.com <adithyanajay@gmail.com>
2023-10-27 21:42:07 +0530

    progress bar setup

--- src/page/DashBoard.jsx
+++ src/page/DashBoard.jsx
@@ -20,16 +20,16 @@ export const DashBoard = () => {
     }
   }, []);
   return (
-    <div className="bg-slate-100 gridsystem  p-8 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6 min-h-full max-h-full overflow-scroll justify-items-center items-center w-full">
-      <WorkBox difficulty={3} />
-      <WorkBox difficulty={2} />
-      <WorkBox difficulty={1} />
-      <WorkBox difficulty={2} />
-      <WorkBox difficulty={3} />
-      <WorkBox difficulty={2} />
-      <WorkBox difficulty={1} />
-      <WorkBox difficulty={2} />
-      <WorkBox difficulty={3} />
+    <div className="bg-slate-100 gridsystem  p-8 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6 min-h-full max-h-full overflow-scroll justify-items-center items-center w-full ">
+      <WorkBox difficulty={3} pro={3} />
+      <WorkBox difficulty={2} pro={5} />
+      <WorkBox difficulty={1} pro={11} />
+      <WorkBox difficulty={2} pro={1} />
+      <WorkBox difficulty={3} pro={12} />
+      <WorkBox difficulty={2} pro={8} />
+      <WorkBox difficulty={1} pro={6} />
+      <WorkBox difficulty={2} pro={4} />
+      <WorkBox difficulty={3} pro={10} />
     </div>
   );
 };
